62d11b8fe3bfda71440436d46f7d8f818a323d72
Chanaka Karunarathne <kmchmk@gmail.com>
2022-09-16 19:25:25 +0530

    Add a mutation to increment the score when the button is pressed

--- ReactApp/src/components/Button/Button.js
+++ ReactApp/src/components/Button/Button.js
@@ -1,14 +1,22 @@
+import { gql, useMutation } from '@apollo/client';
 import React from 'react';
 
+const INCREMENT_MYSCORE = gql`
+    mutation MyMutation {
+        update_player(where: {username: {_eq: "Ravindu"}}, _inc: {score: 1}) {
+        returning {
+            score
+        }
+        }
+    }
+`;
+
 const Button = () => {
-    const [count, setCount] = React.useState(false);
-    const handleClick = () => {
-        setCount(count + 1);
-    };
+    const [incrementMyScore] = useMutation(INCREMENT_MYSCORE);
+
     return (
         <div>
-            <button type="button" onClick={handleClick}>Click Me</button>
-            {count}
+            <button type="button" onClick={incrementMyScore}>Click Me</button>
         </div>
     )
 };
