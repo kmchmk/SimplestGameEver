806e9e40173e948441d9f2abb2969245ed688ccb
Priyantha Madushanka <prmadu93@gmail.com>
2022-09-17 17:55:40 +0530

    my changes

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ExchangeRates } from './high-scores/high-scores.component';
 import { MyscoreComponent } from './myscore/myscore.component';
+import { ButtonComponent } from './button/button.component';
+import { ButtonFunctionComponent } from './button-function/button-function.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExchangeRates,
-    MyscoreComponent
+    MyscoreComponent,
+    ButtonComponent,
+    ButtonFunctionComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularApp/src/app/button-function/button-function.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-function',
+  templateUrl: './button-function.component.html',
+  styleUrls: ['./button-function.component.css']
+})
+export class ButtonFunctionComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
