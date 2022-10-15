07a2e74e1f7a2c2c013efe04bbd23c7240ddb370
Chanaka Karunarathne <kmchmk@gmail.com>
2022-10-15 19:26:18 +0530

    Updates

---
+++ precache-manifest.841d384769ff63fa3a8c36b6834ca783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3464431bc2b284d43cfa",
+    "url": "/SimplestGameEver/static/css/main.26232101.chunk.css"
+  },
+  {
+    "revision": "3464431bc2b284d43cfa",
+    "url": "/SimplestGameEver/static/js/main.5782ca1b.chunk.js"
+  },
+  {
+    "revision": "5d6e98bddc9a9ed3e3f1",
+    "url": "/SimplestGameEver/static/js/runtime~main.267a2ab4.js"
+  },
+  {
+    "revision": "c13d55c4b176fc9e4390",
+    "url": "/SimplestGameEver/static/js/2.40024ede.chunk.js"
+  },
+  {
+    "revision": "f8ad41f4f57d78ab37524b2be734da86",
+    "url": "/SimplestGameEver/index.html"
+  }
+];
